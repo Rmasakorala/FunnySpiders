78ba690d2ede61c84b7a02173d789b6d5527653f
rmasakorala <rmasakorala@deakin.edu.au>
2020-04-08 12:30:35 +1000

    AddedObjectSpawner
    Added in spawner and trying to get touch input to work, having trouble with raycasting.

---
+++ EnhancedTeaching/Assets/Rahul - Sandbox/ObjectSpawner.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Vuforia;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour, ITrackableEventHandler
+{
+    public Vector2 GameArea;
+    public GameObject objectPrefab;
+    private TrackableBehaviour mTrackableBehaviour;
+    private GameObject CurrentAnimal;
+    public GameObject floor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+    }
+
+    private void SpawnObjects ()
+    {
+       // for (int i = 0; i < 3; i++)
+        //{
+            GameObject spawnedObject = Instantiate(objectPrefab) as GameObject;
+
+            spawnedObject.transform.position = new Vector3(floor.transform.position.x,floor.transform.position.y,floor.transform.position.z);
+            CurrentAnimal = spawnedObject;
+       // }
+    }
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED)
+        {
+            SpawnObjects();
+        }
+        else
+        {
+            Destroy(CurrentAnimal);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()   
+    {
+        CurrentAnimal.transform.position = new Vector3((floor.transform.position.x + Random.Range(0.3f,0.3f)), (floor.transform.position.y - 0.5f), (floor.transform.position.z + Random.Range(0.3f, 0.3f)));
+    }
+}
---
+++ EnhancedTeaching/Assets/Rahul - Sandbox/TouchInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchInput : MonoBehaviour
+{
+    private bool isSelected;
+    string selectedObjectName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        for (var i = 0; i < Input.touchCount; ++i)
+        {
+  
+            if (Input.GetKeyDown ("1")) //debuging on PC
+                {
+                    this.transform.Rotate(0, 0, 5);
+                }
+            if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began) //touch input
+                {
+                    Debug.Log("detecting Touch");
+                    Ray ray = Camera.current.ScreenPointToRay(Input.GetTouch(0).position * 100);
+                    Debug.DrawRay(ray.origin, ray.direction * 100, Color.green, 100f);
+                    RaycastHit hit;
+
+                    if (Physics.Raycast(ray, out hit))
+                    {
+                        this.transform.Rotate(0, 0, 5);
+                        selectedObjectName = hit.transform.name;
+                        if(selectedObjectName == this.name)
+                        {
+
+                        }
+                        Debug.Log("selcted object is :" + selectedObjectName);
+
+                    }
+                }
+        }
+    }
+}
