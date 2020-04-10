8704b332210f4dfea97f21fdde7848ced93943c2
Minh <bmtran@deakin.edu.au>
2020-04-10 16:23:24 +1000

    Minh added Photon and UNet to test networking/Attempting to create custom Network GUI/Modified the info button

---
+++ EnhancedTeaching/Assets/Scripts/AddToPlane.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddToPlane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject plane = GameObject.FindGameObjectWithTag("Plane");
+        transform.SetParent(plane.transform, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
